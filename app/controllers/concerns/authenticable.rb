7e4f26ffe1688fd951506624521c172a271d4920
Hantys <pedro.fausto@hotmail.com>
2019-01-06 23:57:10 -0300

    testes com erro

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,5 +2,9 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers["Authorization"])
   end
-  
+
+  def authenticate_with_token!
+    render json: { errors: "Acesso não permitido" }, status: 401 unless current_user.present?
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
